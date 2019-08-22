473cead7cc4b523615a1e49a88c01aa05bff48de
Devarsh shukla <Devarsh14@gmail.com>
2019-08-22 22:08:30 +1000

    angular first service completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ListUserService } from './list-user.service';
+import { UserInfo } from './user-name/user-info';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'APM';
   pageTitle: string = 'Acme proudct Management';
+  users: Array<UserInfo>;
+  constructor(private listUserService: ListUserService){
+    this.users=  this.listUserService.ListOfUsers();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserNameComponent } from './user-name/user-name.component';
+import { ListUserService } from './list-user.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { UserNameComponent } from './user-name/user-name.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ListUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
